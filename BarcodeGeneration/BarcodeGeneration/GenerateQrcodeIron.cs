8b37efeab728020d6d90f9e4a33a4d1ab5fb8e6c
behnamasaei <behnamasaei@gmail.com>
2024-06-29 00:01:44 +0330

    add generate qrcode with iron barcode

---
+++ BarcodeGeneration/BarcodeGeneration/GenerateQrcodeIron.cs
@@ -0,0 +1,57 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using IronBarCode;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+
+namespace BarcodeGeneration;
+
+public class GenerateQrcodeIron
+{
+    public void Run()
+    {
+        int qrPerRow = 4; // تعداد QR کدها در هر سطر
+        int qrPerCol = 6; // تعداد QR کدها در هر ستون
+        int totalQrCodes = qrPerRow * qrPerCol;
+        int qrCodeSize = 130; // اندازه هر QR کد
+        int margin = 10; // فاصله بین QR کدها
+
+        // ایجاد سند PDF
+        PdfDocument document = new PdfDocument();
+        document.Info.Title = "QR Codes";
+
+        // ایجاد صفحه خالی
+        PdfPage page = document.AddPage();
+        page.Size = PdfSharp.PageSize.A4;
+        XGraphics gfx = XGraphics.FromPdfPage(page);
+
+        // تولید QR کدها و قرار دادن آنها در PDF
+        for (int i = 0; i < totalQrCodes; i++)
+        {
+            string guid = Guid.NewGuid().ToString();
+            Bitmap qrCodeImage = GenerateQrCode(guid);
+
+            int xPosition = (i % qrPerRow) * (qrCodeSize + margin);
+            int yPosition = (i / qrPerRow) * (qrCodeSize + margin);
+
+            using (MemoryStream stream = new MemoryStream())
+            {
+                qrCodeImage.Save(stream, ImageFormat.Png);
+                stream.Position = 0;
+                XImage xImage = XImage.FromStream(stream);
+                gfx.DrawImage(xImage, xPosition, yPosition, qrCodeSize, qrCodeSize);
+            }
+        }
+
+        // ذخیره سند با نام منحصر به فرد
+        string filename = $"QRCodes_{DateTime.Now:yyyyMMddHHmmss}.pdf";
+        document.Save(filename);
+        Console.WriteLine($"PDF file '{filename}' generated successfully.");
+    }
+
+
+    static Bitmap GenerateQrCode(string text)
+    {
+        return QRCodeWriter.CreateQrCode(text, 400, QRCodeWriter.QrErrorCorrectionLevel.Highest).ToBitmap();
+    }
+}
--- BarcodeGeneration/BarcodeGeneration/Program.cs
+++ BarcodeGeneration/BarcodeGeneration/Program.cs
@@ -3,6 +3,7 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using BarcodeGeneration;
+using IronBarCode;
 using PdfSharp.Drawing;
 using PdfSharp.Pdf;
 using QRCoder;
@@ -11,10 +12,20 @@ class Program
 {
     static void Main()
     {
-        GenerateBarcode barcode = new GenerateBarcode();
-        barcode.Run();
-        
+        // GenerateBarcode barcode = new GenerateBarcode();
+        // barcode.Run();
+
+
+        IronBarCode.License.LicenseKey = "IRONBARCODE-BOARD4ALL.BIZ-122711-F2BCE7-4CCB5FE5DD-5965DEE1-NEx-EP7";
+
+
+        // bool result = IronBarCode.License.IsValidLicense("IRONBARCODE-BOARD4ALL.BIZ-122711-F2BCE7-4CCB5FE5DD-5965DEE1-NEx-EP7");
+
+        // System.Console.WriteLine(result);
+
+        GenerateQrcodeIron generateQrcodeIron = new();
+        generateQrcodeIron.Run();
     }
 
-    
+
 }
