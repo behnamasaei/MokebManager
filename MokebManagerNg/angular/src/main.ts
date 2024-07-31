9ce705793d5abd84b979ef5fcb01093a750a919e
behnamasaei <behnamasaei@gmail.com>
2024-07-31 23:16:10 +0330

    disable pwa

--- MokebManagerNg/angular/src/main.ts
+++ MokebManagerNg/angular/src/main.ts
@@ -1,6 +1,5 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
@@ -8,11 +7,9 @@ if (environment.production) {
   enableProdMode();
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  platformBrowserDynamic().bootstrapModule(AppModule)
-    .catch(err => console.error(err));
-});
-
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch(err => console.error(err));
 
 // platformBrowserDynamic()
 //   .bootstrapModule(AppModule)
